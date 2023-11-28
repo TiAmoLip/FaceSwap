5ce4af0068d15be8ba14f0f0ee5cdcb324c7601f
TiAmoLip <1239261229@qq.com>
2023-11-28 22:35:20 +0800

    fix

--- models/custom_network.py
+++ models/custom_network.py
@@ -48,7 +48,7 @@ class DeformConvDownSample(nn.Module):
     def __init__(self, in_channels, out_channels, kernel_size, stride=1, padding=0, bias=False):
         super(DeformConvDownSample, self).__init__()
         self.dconv = DeformConv(in_channels, out_channels, kernel_size, stride, padding, bias)
-        self.conv = nn.Conv2d(in_channels, out_channels, kernel_size=1, stride=1, padding=0, bias=False)
+        self.conv = nn.Conv2d(out_channels, out_channels, kernel_size=1, stride=1, padding=0, bias=False)
         self.norm = nn.BatchNorm2d(out_channels)
         self.relu = nn.LeakyReLU(0.2,inplace=True)
 
