ba678c6345ed26bf5aa402a66b2dd09d8c48bb42
TiAmoLip <1239261229@qq.com>
2023-11-29 20:24:04 +0800

    update models

--- models/custom_network.py
+++ models/custom_network.py
@@ -49,11 +49,12 @@ class DeformConvDownSample(nn.Module):
         super(DeformConvDownSample, self).__init__()
         self.dconv = DeformConv(in_channels, out_channels, kernel_size, stride, padding, bias)
         self.conv = nn.Conv2d(out_channels, out_channels, kernel_size=1, stride=1, padding=0, bias=False)
-        self.norm = nn.BatchNorm2d(out_channels)
+        self.norm = nn.InstanceNorm2d(out_channels)
         self.relu = nn.LeakyReLU(0.2,inplace=True)
 
     def forward(self, x):
         x = self.dconv(x)
+        x = self.relu(x)
         x = self.conv(x)
         x = self.norm(x)
         x = self.relu(x)
@@ -65,7 +66,7 @@ class DeformConvUpSample(nn.Module):
         self.upsample = nn.Upsample(scale_factor=scaleFactor, mode='bilinear',align_corners=False)
         self.IdDeformConv = IdDeformConv(latent_size,in_channels, out_channels, kernel_size, stride, padding, bias)
         self.conv = nn.Conv2d(out_channels, out_channels, kernel_size=1, stride=1, padding=0, bias=False)
-        self.norm = nn.BatchNorm2d(out_channels)
+        self.norm = nn.InstanceNorm2d(out_channels)
         self.rl = nn.LeakyReLU(0.2,inplace=True)
     def forward(self, x, latent_id):
         x = self.upsample(x)
@@ -98,7 +99,7 @@ class AFFAModule(nn.Module):
         else:
             self.conv1 = DeformConv(2*in_channels, in_channels, kernel_size=3, stride=1, padding=1)
             self.conv2 = DeformConv(in_channels, in_channels, kernel_size=1, stride=1, padding=0)
-        self.norm = InstanceNorm()
+        self.norm = nn.InstanceNorm2d(in_channels)
         self.act = nn.LeakyReLU(0.2,inplace=True)
     def forward(self, h, z):
 
@@ -157,7 +158,7 @@ class AFFA_RB(nn.Module):
         
         
 class DancerGeneratorEncoder(nn.Module):
-    def __init__(self, input_nc=3) -> None:
+    def __init__(self, input_nc=3,n_layers=3) -> None:
         super().__init__()
         initial_channels = 64
         self.first_layer = nn.Sequential(nn.ReflectionPad2d(3), nn.Conv2d(input_nc, initial_channels, kernel_size=7, padding=0))
@@ -168,21 +169,25 @@ class DancerGeneratorEncoder(nn.Module):
         self.down = nn.ModuleList()
         for i in range(3):
             self.down.append(DeformConvDownSample(initial_channels*(2**i),initial_channels*(2**(i+1)),kernel_size=3,stride=2,padding=1))
+        for i in range(n_layers-3):
+            self.down.append(DeformConvDownSample(512,512,kernel_size=3,stride=1,padding=1))
     def forward(self,x):
         features = []
         x = self.first_layer(x)
         for i in range(len(self.down)):
-
             x = self.down[i](x)
             features.append(x)
+        # for i in range()
         features.reverse()# ensure that the first element is the output last layer
         return x,features
     
 class DancerGeneratorDecoder(nn.Module):
-    def __init__(self,latent_size, kernel_type, upsample_method) -> None:
+    def __init__(self,latent_size, n_layers, kernel_type, upsample_method) -> None:
         super().__init__()
         final_channels = 512
         self.up = nn.ModuleList()
+        for i in range(n_layers-3):
+            self.up.append(AFFA_RB(latent_size=latent_size,in_channels=512,out_channels=512,sample_method="none",upsample_method=upsample_method,kernel_type=kernel_type))
         for i in range(3):
             self.up.append(AFFA_RB(latent_size=latent_size,in_channels=final_channels//(2**i),out_channels=final_channels//(2**(i+1)),sample_method="up",upsample_method=upsample_method,kernel_type=kernel_type))
         self.last_layer = nn.Sequential(nn.ReflectionPad2d(3), nn.Conv2d(final_channels//8, 3, kernel_size=7, padding=0))
@@ -194,14 +199,14 @@ class DancerGeneratorDecoder(nn.Module):
         return x
 
 class DancerGenerator(nn.Module):
-    def __init__(self, input_nc=3, output_nc=3, latent_size=512, n_blocks=6, deep=False,norm_layer=nn.BatchNorm2d,padding_type='reflect',
+    def __init__(self, input_nc=3, output_nc=3, latent_size=512, n_blocks=6, n_layers = 3, deep=False,norm_layer=nn.InstanceNorm2d,padding_type='reflect',
                  kernel_type="ordinary",upsample_method="ordinary") -> None:
         assert (n_blocks >= 0)
         super(DancerGenerator, self).__init__()
         self.deep = deep
         
-        self.enc = DancerGeneratorEncoder(input_nc)
-        
+        self.enc = DancerGeneratorEncoder(input_nc, n_layers)
+        self.enc_norm = norm_layer(512)
         BN = []
         activation = nn.LeakyReLU(0.2,True)
         for i in range(n_blocks):
@@ -211,13 +216,14 @@ class DancerGenerator(nn.Module):
         
         self.transition = nn.Conv2d(512, 512, kernel_size=3, stride=1, padding=1)
         
+        self.dec_norm = norm_layer(512)
         
         # self.affa3 = AFFA_RB(latent_size = 512,in_channels = 512,out_channels= 256, sample_method="up",kernel_type=kernel_type,upsample_method=upsample_method)
         # self.affa2 = AFFA_RB(latent_size= 512,in_channels= 256,out_channels= 128, sample_method="up",kernel_type=kernel_type,upsample_method=upsample_method)
         # self.affa1 = AFFA_RB(latent_size = 512,in_channels= 128,out_channels= 64, sample_method="up",kernel_type=kernel_type,upsample_method=upsample_method)
-        self.dec = DancerGeneratorDecoder(latent_size,kernel_type,upsample_method)
+        self.dec = DancerGeneratorDecoder(latent_size, n_layers ,kernel_type,upsample_method)
         
-        self.norm = InstanceNorm()
+        self.norm = nn.InstanceNorm2d(512)
         self.last_layer = nn.Sequential(nn.ReflectionPad2d(3), DeformConv(64, output_nc, kernel_size=7, padding=0))
     def forward(self, x, latent):
         # x: (batch_size, 3, 224, 224)
